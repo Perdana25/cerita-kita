57e2c57009046d27cea58959b32de33e746dbe42
Perdana25 <217666492+Perdana25@users.noreply.github.com>
2026-02-14 12:05:57 -0500

    Fix: Final syntax fix for stories.js

--- data/stories.js
+++ data/stories.js
@@ -3,14 +3,15 @@ window.stories = [
         id: 1,
         title: "Jatuh Hati",
         date: "14 Januari 2024",
-        dateRaw: "2024-1-14",
+        dateRaw: "2024-01-14",
         shortText: "Hari itu dimana aku pertama kali jatuh hati denganmu.",
         // Use backticks (`) for multi-line text (Like Word!)
         fullText: `
     Hari ini adalah hari pertama kali aku benar - benar melihatmu, hari dimana kita berkendara berdua di tengah malam menghilangkan sedikit penat kita dari hirau piruk nya kegiatan saat itu,
 malam itu aku melarangmu untuk pergi tanpa diriku, kita berdua keluar dari desa itu dan mengejar salah satu kawan kita di kegelapan malam itu yang dimana itu terasa dingin menusuk dan menakutkan karena gelapnya penerangan disana.
 Melihat dirimu yang begitu percaya padaku dan mengandalkanku, kamu begitu senang saat itu walau kedinginan, dari situ kita sering keluar malam sebelum rapat berdua untuk ikut makan di luar sana. malam itu sangat indah walau cuma sebentar.
-indahnya dirimu, senyum mu dan bahagia mu saat itu membuat hariku lebih baik.
+indahnya dirimu, senyum mu and bahagia mu saat itu membuat hariku lebih baik.
         `,
         image: "assets/stories/1.jpg"
     }
+];
